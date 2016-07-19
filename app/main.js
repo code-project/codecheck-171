b805d3fc165e98692f7ae7f009df2cd30c71f44c
devcheck <dev@code-check.io>
2016-07-19 21:23:00 +0900

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,15 @@
 "use strict";
 
+function fizzbuzz(n) {
+  if (n % 15 === 0) return "FizzBuzz";
+  if (n % 5 === 0)  return "Buzz";
+  if (n % 3 === 0)  return "Fizz";
+  return n.toString();
+}
+
 function main(args, options) {
-  args.forEach(v => {
-    console.log(v);
-  });
+  var n = parseInt(args[0]);
+  console.log(fizzbuzz(n));
 }
 
 module.exports = main;
